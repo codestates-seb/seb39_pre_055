c6752cf871388df9e31e0bad049d2782c81ec0b5
ksheer506 <ksheer506@naver.com>
2022-08-24 20:04:09 +0900

    #25 fix: DefaultButton export 방식 변경

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,6 +1,4 @@
-import Button from './Button/MainAuth/MainAuth';
-
-export { Button };
+export { default as DefaultBtn } from './Button/DefaultButton';
 export { default as SharedLayout } from './SharedLayout/SharedLayout';
 export { default as Tag } from './Tag/Tag';
 export { default as Triangle } from './Triangle/Triangle';
