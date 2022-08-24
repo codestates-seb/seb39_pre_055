b6b43a8cf57316b445ce68b76d09a5400fe9177f
ksheer506 <ksheer506@naver.com>
2022-08-24 14:54:22 +0900

    test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ReactComponent as Sprites } from './assets/img/sprites.svg';
 import { SharedLayout } from './components';
 import { AskQuestion, QuestionDetail, QuestionList } from './pages';
-
+const test = "test"
 const App = () => {
   return (
     <BrowserRouter>
