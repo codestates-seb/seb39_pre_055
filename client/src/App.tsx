94a1bab7eb0485aae01885b3642bf60d240152aa
ksheer506 <ksheer506@naver.com>
2022-08-24 17:36:49 +0900

    design: Footer 컴포넌트 스타일링

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { ReactComponent as Sprites } from './assets/img/sprites.svg';
 import { SharedLayout } from './components';
 import { AskQuestion, QuestionDetail, QuestionList } from './pages';
-const test = "test"
+
 const App = () => {
   return (
     <BrowserRouter>
