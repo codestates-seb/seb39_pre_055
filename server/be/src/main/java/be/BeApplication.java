a4e43dc546c10d8ebd081d51d6ca30c8a63119d1
soyoung96 <psy411301@naver.com>
2022-08-24 17:36:00 +0900

    psy

---
+++ server/be/src/main/java/be/BeApplication.java
@@ -0,0 +1,13 @@
+package be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeApplication.class, args);
+	}
+
+}
