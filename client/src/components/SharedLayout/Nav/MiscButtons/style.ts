8181a7af23744871d8fb7ceddb04a534535697f3
ksheer506 <ksheer506@naver.com>
2022-08-25 19:48:56 +0900

    #25 style: Nav바 Misc 버튼 반응형 완성

--- client/src/components/SharedLayout/Nav/MiscButtons/style.ts
+++ client/src/components/SharedLayout/Nav/MiscButtons/style.ts
@@ -1,7 +1,32 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+import { Theme } from '../../../../types/globalStyle';
+import { RoundButton } from '../../../Button/Templates';
 
 export const Misc = styled.div`
   display: flex;
   column-gap: 5px;
   margin-left: 10px;
+`;
+
+type HidableProps = {
+  isLoggedIn?: boolean;
+} & Theme;
+
+export const Hideable = styled(RoundButton)`
+  ${({ theme, isLoggedIn }: HidableProps) => css`
+    display: ${isLoggedIn ? 'none' : ''};
+
+    @media screen and (max-width: ${theme.breakPoints.tablet}) {
+      display: none;
+    }
+  `}
+`;
+
+export const Products = styled(RoundButton)`
+  ${({ theme }) => css`
+    @media screen and (max-width: ${theme.breakPoints.tablet}) {
+      font-size: 0.8rem;
+    }
+  `}
 `;
