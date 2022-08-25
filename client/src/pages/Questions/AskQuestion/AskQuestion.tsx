c471d85861c788432d4187b78dc3d5ea2941199a
ksheer506 <ksheer506@naver.com>
2022-08-25 17:38:15 +0900

    test: 검색바 컴포넌트 테스트

--- client/src/pages/Questions/AskQuestion/AskQuestion.tsx
+++ client/src/pages/Questions/AskQuestion/AskQuestion.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import { SearchBar } from '../../../components';
+
 const AskQuestion = () => {
-  return <div>AskQuestion</div>;
+  return (
+    <div>
+      <SearchBar placeholder="test" width="300px" height="40px" />
+    </div>
+  );
 };
 
 export default AskQuestion;
