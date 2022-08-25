90bfa117256b35680630360f49462f405be98c6f
ksheer506 <ksheer506@naver.com>
2022-08-25 17:41:17 +0900

    #25 test: 기타 버튼 반응형 테스트

--- client/src/components/SharedLayout/Nav/MiscButtons/MiscBtns.tsx
+++ client/src/components/SharedLayout/Nav/MiscButtons/MiscBtns.tsx
@@ -1,18 +1,36 @@
+import styled, { css } from 'styled-components';
+
+import { Theme } from '../../../../types/globalStyle';
 import { RoundButton } from '../../../Button/Templates';
 import { Misc } from './style';
 
+type HidableProps = {
+  isLoggedIn?: boolean;
+} & Theme;
+
+const Hideable = styled(RoundButton)`
+  ${({ theme, isLoggedIn }: HidableProps) => css`
+    display: ${isLoggedIn ? 'none' : ''};
+
+    @media screen and (max-width: ${theme.breakPoints.tablet}) {
+      display: none;
+    }
+  `}
+`;
+
 const MiscBtns = () => {
   return (
+    // TODO: 로그인 상태에 따라 안 보이게 설정
     <Misc>
-      <RoundButton color="black" mainCode="025" hoverCode="075">
+      <Hideable color="black" mainCode="025" hoverCode="075">
         About
-      </RoundButton>
+      </Hideable>
       <RoundButton color="black" mainCode="025" hoverCode="075">
         Products
       </RoundButton>
-      <RoundButton color="black" mainCode="025" hoverCode="075">
+      <Hideable color="black" mainCode="025" hoverCode="075">
         For Teams
-      </RoundButton>
+      </Hideable>
     </Misc>
   );
 };
