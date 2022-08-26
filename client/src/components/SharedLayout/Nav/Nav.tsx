b4e5f3fb5966b74a077d3539925aa7092d9e7a8b
ksheer506 <ksheer506@naver.com>
2022-08-26 18:26:14 +0900

    chore: Nav, MainSearchBar 컴포넌트 파일 분리

--- client/src/components/SharedLayout/Nav/Nav.tsx
+++ client/src/components/SharedLayout/Nav/Nav.tsx
@@ -1,77 +1,24 @@
-/* eslint-disable consistent-return */
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import { SearchBar } from '../..';
-import { useModal } from '../../Modal';
-import MainAuth from './MainAuth/MainAuth';
+import { Hamburger } from '../..';
+import MiniSearchBar from '../../SearchBar/MiniSearchBar';
+import MainAuth from './MainAuthButtons/MainAuthButtons';
+import SearchBarBox from './MainSearchBar/MainSearchBar';
 import MiscBtns from './MiscButtons/MiscBtns';
-import { LogoWrapper, MainLogo, SHeader, SNav } from './style';
-
-const SearchHints = () => {
-  return <div>TEtwetsegysgwse</div>;
-};
-
-const MainSearchBar = () => {
-  const [position, setPosition] = useState({ x: '500px', y: '51px' });
-  const [size, setSize] = useState({ width: '200px', height: '100px' });
-  const [isFocused, setIsFocused] = useState(false);
-  const { openModal, closeModal } = useModal(size, position);
-  const searchRef = useRef<HTMLInputElement>(null);
-
-  const resizeModal = useCallback(() => {
-    if (!searchRef.current) return;
-
-    const left = searchRef.current.offsetLeft;
-    const width = searchRef.current.offsetWidth;
-
-    setPosition((prev) => {
-      return { ...prev, x: `${left}px` };
-    });
-    setSize((prev) => {
-      return { ...prev, width: `${width}px` };
-    });
-  }, []);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (!isFocused) return;
-      resizeModal();
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => window.removeEventListener('resize', handleResize);
-  }, [isFocused, resizeModal]);
-
-  // 1. resize 없이 처음 Modal을 열었을 때 사이즈가 useState() 초기값 그대로인 문제 수정
-  /* useEffect(() => resizeModal(), [resizeModal]); */
-
-  return (
-    <SearchBar
-      placeholder="Search"
-      onFocus={() => {
-        resizeModal();
-        setIsFocused(true);
-        openModal(<SearchHints />);
-      }}
-      onBlur={() => {
-        setIsFocused(false);
-        closeModal();
-      }}
-      wrapperRef={searchRef}
-    />
-  );
-};
+import { LogoBox, MainLogoSVG, SHeader, SNav } from './style';
 
 const Nav = () => {
   return (
     <SHeader>
       <SNav>
-        <LogoWrapper>
-          <MainLogo />
-        </LogoWrapper>
+        <Hamburger />
+        <LogoBox>
+          <Link to="/" />
+          <MainLogoSVG />
+        </LogoBox>
         <MiscBtns />
-        <MainSearchBar />
+        <SearchBarBox />
+        <MiniSearchBar />
         <MainAuth />
       </SNav>
     </SHeader>
