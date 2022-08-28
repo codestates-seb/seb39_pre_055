44db323cacf6b6d2143080bfb51b400f82ae20f2
so191925 <so191925@gmail.com>
2022-08-29 01:05:28 +0900

    #51 refactor: 스타일코드 분리

--- client/src/components/QuestionElement/LeftCounts/LeftCounts.tsx
+++ client/src/components/QuestionElement/LeftCounts/LeftCounts.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import { Container, Wrapper, Wrapper2, Wrapper3 } from './style';
 
 interface Prop {
   votes: number;
@@ -6,52 +6,12 @@ interface Prop {
   views: number;
 }
 
-const Wrapper = styled.span`
-  display: flex;
-  justify-content: flex-end;
-  font-size: 11px;
-  color: var(--fc-dark);
-  margin-top: 17px;
-  width: 40px;
-`;
-
-const Wrapper2 = styled.span`
-  display: flex;
-  justify-content: flex-end;
-  font-size: 11px;
-  color: var(--black-500);
-  margin-top: 7px;
-  width: 70px;
-`;
-
-const Wrapper3 = styled.span`
-  display: flex;
-  justify-content: flex-end;
-  font-size: 11px;
-  color: var(--black-500);
-  margin-top: 7px;
-`;
-
-const Container = styled.span`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  width: 150px;
-`;
-
-// const TextContainer = styled.span`
-//   flex-direction: column;
-//   align-items: flex-start;
-// `;
-
 const LeftCounts = ({ votes, answers, views }: Prop) => {
   return (
     <Container>
-      {/* <TextContainer> */}
       <Wrapper>{votes} votes</Wrapper>
       <Wrapper2>{answers} answers</Wrapper2>
       <Wrapper3>{views} views</Wrapper3>
-      {/* </TextContainer> */}
     </Container>
   );
 };
