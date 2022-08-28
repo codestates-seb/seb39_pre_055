0a2f00be1c1607c275433d6bb637c3a705844709
MoSangBin <mosangbin94@gmail.com>
2022-08-28 23:34:11 +0900

    #56 style: sidebar 추가

--- client/src/pages/Edit/EditQuestion.tsx
+++ client/src/pages/Edit/EditQuestion.tsx
@@ -88,6 +88,36 @@ const CancelButton = styled.button`
   }
 `;
 
+const Sidebar = styled.div`
+  aside {
+    width: 360px;
+    margin: 0 auto;
+    margin-top: 30px;
+    border: 1px solid hsl(47, 65%, 84%);
+    background-color: hsl(47, 83%, 91%);
+    color: rgb(59, 64, 69);
+    box-shadow: 0 1px 2px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05),
+      0 2px 8px hsla(0, 0%, 0%, 0.05);
+  }
+
+  header {
+    padding: 12px 15px;
+    border-bottom: 1px solid hsl(47, 65%, 84%);
+    font-size: 15px;
+  }
+
+  ul {
+    padding: 4px 15px;
+    background-color: #faf5e6;
+  }
+
+  li {
+    margin: 12px 0;
+    font-size: 13px;
+    list-style: inside;
+  }
+`;
+
 const EditQuestion = () => {
   const editorRef = useRef<Editor>(null);
   // const dispatch = useAppDispatch();
@@ -183,9 +213,9 @@ const EditQuestion = () => {
       </HashTagContainer>
       <BlueButton width="90px">Save Edits</BlueButton>
       <CancelButton>Cancel</CancelButton>
-      <div>
+      <Sidebar>
         <aside>
-          <h2>How to Edit</h2>
+          <header>How to Edit</header>
           <ul>
             <li>Correct minor typos or mistakes</li>
             <li>Clarify meaning without changing it</li>
@@ -194,7 +224,7 @@ const EditQuestion = () => {
             <li>Don’t use edits to reply to the author</li>
           </ul>
         </aside>
-      </div>
+      </Sidebar>
     </Container>
   );
 };
