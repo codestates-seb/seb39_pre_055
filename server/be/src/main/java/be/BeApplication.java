5c9b9aca19abdcf4934926cff118f8795a339c2c
soyoung96 <psy411301@naver.com>
2022-08-29 11:40:16 +0900

    test2

--- server/be/src/main/java/be/BeApplication.java
+++ server/be/src/main/java/be/BeApplication.java
@@ -2,6 +2,9 @@ package be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BeApplication {
@@ -10,4 +13,13 @@ public class BeApplication {
 		SpringApplication.run(BeApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://13.209.17.182");
+			}
+		};
+	}
 }
