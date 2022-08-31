a43125df0f0a5667dcaefd8cf6420bf3021fbf4a
ksheer506 <ksheer506@naver.com>
2022-08-31 18:58:34 +0900

    fix: useLocalStorage JSON 파싱 오류 수정

--- client/src/hooks/useLocalStorage.ts
+++ client/src/hooks/useLocalStorage.ts
@@ -2,10 +2,10 @@ import { useState } from 'react';
 
 export function useLocalStorage<T>(
   key: string,
-  initialValue: T
+  initialValue?: T
 ): [T, (x: T) => void] {
   const [data, setData] = useState<T>(() => {
-    const storageData = JSON.parse(localStorage.getItem(key) || '');
+    const storageData = JSON.parse(localStorage.getItem(key) || '""');
 
     return storageData || initialValue;
   });
