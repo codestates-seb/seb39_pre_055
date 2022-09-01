a15c4bf7ff90023247b4ff528ce15ad8ae3fa1e6
MoSangBin <mosangbin94@gmail.com>
2022-09-01 22:38:06 +0900

    chore: constants 폴더 생성, 분리

--- client/src/pages/Register/Login/Login.tsx
+++ client/src/pages/Register/Login/Login.tsx
@@ -1,23 +1,17 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 import { ReactComponent as Sprites } from '../../../assets/img/sprites.svg';
+import { BlueButton, DefaultInput } from '../../../components';
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 1000px;
+  width: 100%;
+  height: calc(100vh - 50px);
 `;
-
-const Login = () => {
-  return (
-    <Container>
-      <BottomIconSVG />
-    </Container>
-  );
-};
-
-export default Login;
 
 export const BottomIconSVG = styled(Sprites).attrs({
   viewBox: '0 0 28 50',
@@ -25,3 +19,53 @@ export const BottomIconSVG = styled(Sprites).attrs({
   width: 50px;
   height: 60px;
 `;
+
+export const LoginCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 300px;
+  height: 245px;
+  padding: 24px;
+  margin: 24px 0;
+  background-color: white;
+  border-radius: 7px;
+  box-shadow: 0 10px 24px hsla(0, 0%, 0%, 0.05),
+    0 20px 48px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.1);
+`;
+
+const Login = () => {
+  const [email, setEmail] = useState({
+    value: '',
+    isError: false,
+  });
+  const [password, setPassword] = useState({
+    value: '',
+    isError: false,
+  });
+
+  return (
+    <Container>
+      <BottomIconSVG />
+      <LoginCard>
+        <DefaultInput
+          label="Email"
+          id="email"
+          value={email.value}
+          isError={email.isError}
+          onChange={(e) => console.log(e.target.value)}
+        />
+        <DefaultInput
+          label="Password"
+          id="password"
+          value={password.value}
+          isError={password.isError}
+          onChange={(e) => console.log(e.target.value)}
+        />
+        <BlueButton height="35px">Log in</BlueButton>
+      </LoginCard>
+    </Container>
+  );
+};
+
+export default Login;
--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,5 +1,5 @@
+export * from '../constants/regex';
 export * from './axiosInstance';
 export * from './formatter';
 export * from './markdown-data';
-export * from './regex';
 export * from './tag-data';
