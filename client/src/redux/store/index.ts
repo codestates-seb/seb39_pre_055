a896e9002e0d5515cffd8a9733da9f17f511ec6a
ksheer506 <ksheer506@naver.com>
2022-09-01 12:07:05 +0900

    #137 feat: searchSlice 초기 구성

--- client/src/redux/store/index.ts
+++ client/src/redux/store/index.ts
@@ -8,6 +8,7 @@ import {
   userReducer,
 } from '../reducers';
 import { questionReducer2 } from '../reducers/questionSlice2';
+import { searchReducer } from '../reducers/searchSlice';
 
 export const store = configureStore({
   reducer: {
@@ -16,6 +17,7 @@ export const store = configureStore({
     tag: tagReducer,
     questionSort: questionSortReducer,
     user: userReducer,
+    search: searchReducer,
     // 앞으로 추가하게 될 전역 상태는 관심사에 따라 파일을 분리한 후 이곳에 추가해주세요.
     // user: userReducer,  -> user정보에 관련된 전역 상태
     // mountain: mountainReducer,  -> 산과 관련된 전역 상태
