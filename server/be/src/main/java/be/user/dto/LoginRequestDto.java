b7941bdfa9db0ae247babc993298843aef1d5d79
쏘영 <61177857+soyoung96@users.noreply.github.com>
2022-09-02 23:47:56 +0900

    Delete server directory

--- server/be/src/main/java/be/user/dto/LoginRequestDto.java
+++
@@ -1,9 +0,0 @@
-package be.user.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequestDto {
-    private String email;
-    private String password;
-}
--- server/be/src/main/java/be/user/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package be.user.repository;
-
-import be.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email); // JPA query method. PrincipalDetailsService에 사용될 메서드 (프리 플젝에서는 username 대신에 email 사용)
-}
