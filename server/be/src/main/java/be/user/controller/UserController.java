999a45df8f88fe45d8ebc64fb593260abc8b3cb5
soyoung96 <psy411301@naver.com>
2022-09-04 11:48:15 +0900

    #190 토큰에 대한 유저정보 반환 API 구현

--- server/be/src/main/java/be/user/controller/UserController.java
+++ server/be/src/main/java/be/user/controller/UserController.java
@@ -9,10 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -39,6 +36,17 @@ public class UserController {
                 HttpStatus.CREATED);
     }
 
-//    @PostMapping("/login")
+    /**
+     * 토근에 해당하는 User 정보를
+     * 클라이언트에게 전달
+     **/
+    @GetMapping("/user")
+    public ResponseEntity getUser(){
+        User user =  userService.getLoginUser();
+
+        return new ResponseEntity<>(
+                new SingleResponseDto<>(mapper.userToUserResponseDto(user)),
+                HttpStatus.OK);
+    }
 
 }
