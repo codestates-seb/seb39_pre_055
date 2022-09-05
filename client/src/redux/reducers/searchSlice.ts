e763a0f6b40ed8c33d0b50ec641bc83fe8a120e0
ksheer506 <ksheer506@naver.com>
2022-09-05 14:41:20 +0900

    feat: Search Slice 타입 및 로직 수정

--- client/src/redux/reducers/searchSlice.ts
+++ client/src/redux/reducers/searchSlice.ts
@@ -1,64 +1,32 @@
 import { createSlice, PayloadAction, Reducer } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
 
+import { Question } from '../../types/question';
 import { getSearchResults } from '../actions/searchActions';
 import { RootState } from '../store';
-
-export interface SearchResults {
-  title: string;
-  body: string;
-  tag: string[];
-  vote: number;
-  view: number;
-  display_name: string;
-  profile_image: string;
-  created_at: string;
-  updated_at: string;
-  question_id: number;
-  user_id: number;
-}
 
 interface Search {
   keyword: string;
   page: number;
+  totalElements: number;
+  totalPages: number;
+  size: number;
+  sortOption: string;
   isLoading: boolean;
   errorMsg: unknown;
-  result: SearchResults[];
+  result: Question[];
 }
 
 const initialState: Search = {
-  keyword: 'react',
+  keyword: '',
   page: 1,
-  isLoading: true,
-  errorMsg: 'err',
-  result: [
-    {
-      title: 'java is difficult',
-      body: '도와주세요~! help~',
-      tag: ['java', 'javascript'],
-      vote: 20,
-      view: 321,
-      display_name: 'dvlp',
-      profile_image: 'https://www.a.com/image/good.jpg',
-      created_at: '2022-08-24',
-      updated_at: '2022-08-25',
-      question_id: 2,
-      user_id: 5,
-    },
-    {
-      title: 'js is also...',
-      body: '또 도와주세요~! help, plz~',
-      tag: ['java', 'javascript'],
-      vote: 19,
-      view: 311,
-      display_name: 'cdr',
-      profile_image: 'https://www.c.com/image/better.jpg',
-      created_at: '2022-08-25',
-      updated_at: '2022-08-26',
-      question_id: 2,
-      user_id: 5,
-    },
-  ],
+  totalElements: 0,
+  totalPages: 1,
+  size: 14,
+  sortOption: 'votes',
+  isLoading: false,
+  errorMsg: '',
+  result: [],
 };
 
 const searchSlice = createSlice({
@@ -68,7 +36,13 @@ const searchSlice = createSlice({
     setKeyword: (state, { payload }: PayloadAction<string>) => {
       state.keyword = payload;
     },
-    setResults: (state, { payload }: PayloadAction<SearchResults[]>) => {
+    changeQPage: (state, { payload }: PayloadAction<number>) => {
+      state.page = payload;
+    },
+    changeQSortOption: (state, { payload }: PayloadAction<string>) => {
+      state.sortOption = payload;
+    },
+    setResults: (state, { payload }: PayloadAction<Question[]>) => {
       state.result = payload;
     },
   },
@@ -77,31 +51,31 @@ const searchSlice = createSlice({
       .addCase(getSearchResults.pending, (state) => {
         state.isLoading = true;
       })
-      .addCase(
-        getSearchResults.fulfilled,
-        (state, { payload }: PayloadAction<SearchResults[]>) => {
-          state.isLoading = false;
-          state.result = payload;
-        }
-      )
-      .addCase(
-        getSearchResults.rejected,
-        (state, { payload }: PayloadAction<unknown>) => {
-          state.isLoading = false;
-          state.errorMsg = payload;
+      .addCase(getSearchResults.fulfilled, (state, { payload }) => {
+        const { data, pageInfo } = payload;
+        const { totalElements, totalPages } = pageInfo;
 
-          if (typeof payload === 'string') {
-            toast.error(payload);
-          }
+        state.isLoading = false;
+        state.result = data;
+        state.totalElements = totalElements;
+        state.totalPages = totalPages;
+      })
+      .addCase(getSearchResults.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        state.errorMsg = payload;
+
+        if (typeof payload === 'string') {
+          toast.error(payload);
         }
-      ),
+      }),
 });
 
 /* Selectors */
 export const selectResultIds = (state: RootState) =>
-  state.search.result.map((q) => q.question_id);
+  state.search.result.map((q) => q.questionId);
 export const selectInfos = (state: RootState, id: number) =>
-  state.search.result.filter((q) => q.question_id === id)[0];
+  state.search.result.filter((q) => q.questionId === id)[0];
 
-export const { setKeyword, setResults } = searchSlice.actions;
+export const { setKeyword, setResults, changeQPage, changeQSortOption } =
+  searchSlice.actions;
 export const searchReducer: Reducer<Search> = searchSlice.reducer;
