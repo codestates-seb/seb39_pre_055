59481aed3f4ed9623fbe8714a603c7ab0693b5a7
ksheer506 <ksheer506@naver.com>
2022-09-06 21:22:47 +0900

    feat: 질문 작성 페이지 반응형 UI 설정

--- client/src/pages/Questions/AskQuestion/style.ts
+++ client/src/pages/Questions/AskQuestion/style.ts
@@ -5,33 +5,42 @@ import { ReactComponent as Robot } from '../../../assets/img/background.svg';
 export const SQuestionBox = styled.div`
   display: flex;
   flex-flow: column wrap;
+  align-items: center;
   padding: 0px 15px;
-  height: 1170px;
-`;
 
-export const SHelpAside = styled.aside``;
+  @media screen and (max-width: ${(props) => props.theme.breakPoints.mobile}) {
+    column-gap: 0px;
+  }
+`;
 
 export const STitleHeader = styled.header`
   display: flex;
   flex-flow: row nowrap;
   align-items: center;
+  width: 100%;
+  max-width: 1100px;
 `;
 
 export const STitleH1 = styled.h1`
+  display: block;
   font-size: 2rem;
-  padding: 40px 30px;
+  padding: 40px 10px 40px 30px;
+  margin-right: auto;
 `;
 
 export const SRobotSVG = styled(Robot)`
-  margin-left: auto;
   width: 600px;
+
+  @media screen and (max-width: ${(props) => props.theme.breakPoints.tablet}) {
+    display: none;
+  }
 `;
 
 export const SBox = styled.div`
   display: flex;
   flex-flow: row nowrap;
   justify-content: center;
-  max-width: 1600px;
+  /* flex: 1 0 100%; */
   column-gap: 20px;
 `;
 
@@ -40,4 +49,8 @@ export const SCardAside = styled.aside`
   flex-flow: column nowrap;
   row-gap: 30px;
   max-height: 80vh;
+
+  @media screen and (max-width: ${(props) => props.theme.breakPoints.tablet}) {
+    display: none;
+  }
 `;
